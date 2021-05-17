e8ee16afcce98ce7cb08719c1d6cd52071d04c74
Ganeshverma123 <ganeshnv0@gmail.com>
2021-05-18 00:45:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017/blog', {
+mongoose.connect('mongodb+srv://ganeshverma:Ganesh@2000@cluster0.58ofe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
